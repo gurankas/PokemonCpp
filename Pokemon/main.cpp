f83f2c9b81a5e87321f0815d8304d2eb97868dc7
gurankas <gurankas@gmail.com>
2025-04-04 20:28:33 -0700

    Added Intro message to the main function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
+using namespace std;
+
+string player_name = "";
+
+int main() 
+{
+    cout << "Trainer! Before you embark on your journey," << endl
+        <<"the Pokémon world needs to know who you are!"<<endl<<endl
+        <<"Time to step up and tell us your name." <<endl
+        <<"And remember, a great name is the start of every great adventure!"<< endl;
+    cin >> player_name;
+    cout << "Great start, Trainer! Looks like you’ve mastered the opening of every code adventure."<<endl
+        <<"Keep this up, and you’ll be a coding master in no time!" << endl;
 
     return 0;
 }
